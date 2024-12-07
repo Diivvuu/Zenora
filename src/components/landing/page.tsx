2dd945a7a432012f85b0c50ce0be8cace14bfe64
Divyanshu Bawa <Divyanshu.12014266@lpu.in>
2024-12-07 21:56:44 +0530

    landing page bg

--- src/components/landing/page.tsx
+++ src/components/landing/page.tsx
@@ -3,29 +3,35 @@ import React, { useRef } from "react";
 import { useScroll, useTransform, motion } from "framer-motion";
 import { AuroraBackground } from "../ui/aurora-background";
 import SearchBar from "../ui/searchBar";
-import { HeroParallax } from "../ui/hero-parallax";
-import { products } from "@/constants/data";
 import { BackgroundBeams } from "../ui/background-beams";
 
 const LandingPage = () => {
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end start"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], ["0vh", "150vh"]);
   return (
-    <>
-      <AuroraBackground showRadialGradient={true}>
-        <BackgroundBeams />
-        <div className="z-10 mb-5 relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0 section-padding  h-[100vh] flex flex-col justify-center">
-          <div className="mb-[5rem]">
-            <h1 className="primary-heading">
-              Welcome to <br />
-              Zenora
-            </h1>
+    <div className="overflow-hidden h-screen">
+      <motion.div style={{ y }} className="relative h-full">
+        <AuroraBackground showRadialGradient={true}>
+          <BackgroundBeams />
+          <div className="z-10 mb-5 relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0 section-padding  h-[100vh] flex flex-col justify-center">
+            <div className="mb-[5rem]">
+              <h1 className="primary-heading">
+                Welcome to <br />
+                Zenora
+              </h1>
+            </div>
+            <div>
+              <SearchBar />
+            </div>
           </div>
-          <div>
-            <SearchBar />
-          </div>
-        </div>
-      </AuroraBackground>
-      {/* <HeroParallax products={products} /> */}
-    </>
+        </AuroraBackground>
+        {/* <HeroParallax products={products} /> */}
+      </motion.div>
+    </div>
   );
 };
 
