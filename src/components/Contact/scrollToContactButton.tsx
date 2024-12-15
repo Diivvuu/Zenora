4bc568babf501dc6553820b3d6ade1144b8dc50b
Shailly-Sahay <shaillysahayy@gmail.com>
2024-12-15 19:48:57 +0530

    contact button

--- src/components/Contact/scrollToContactButton.tsx
+++ src/components/Contact/scrollToContactButton.tsx
@@ -12,8 +12,8 @@ function ScrollToContactButton() {
   return (
     <div className="text-white w-fit fixed bottom-20 right-20 z-10">
       <div onClick={scrollToContact} style={{ cursor: "pointer" }}>
-        <div className="border-[3px] border-yellow-600 rounded-full p-3">
-          <Mail className="text-yellow-600" />
+        <div className="border-[3px] border-[var(--color-primary-500)] rounded-full p-3">
+          <Mail className="text-[var(--color-primary-500)]" />
         </div>
       </div>
     </div>
